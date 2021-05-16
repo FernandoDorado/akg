8c9ae4cbdcd6a2017f629d5139be16b1c76b45af
adamkgray <adamkgray@pm.me>
2021-05-16 14:53:16 +0800

    change role to cluster role

--- k8s/akg/install.sh
+++ k8s/akg/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 kubectl apply -f vs.yml
-kubectl apply -f role.yml
-kubectl apply -f rolebinding.yml
+kubectl apply -f cluster-role.yml
+kubectl apply -f cluster-role-binding.yml
 kubectl apply -f sa.yml
 kubectl apply -f ksvc.yml
