b96304d3f267deff96dea30dd24b7a7e3ceae294
Adam K Gray <adamkgray@pm.me>
2021-05-22 10:46:46 +0800

    let VS code format the code

--- containers/akg/main.go
+++ containers/akg/main.go
@@ -1,26 +1,25 @@
 package main
 
 import (
-    "log"
+	"log"
 
-    "akg/webserver"
-    "akg/k8s"
+	"akg/k8s"
+	"akg/webserver"
 )
 
 func main() {
-    log.Print("debug begin")
+	log.Print("debug begin")
 
-    // k8s
-    k := &k8s.K8s{
-        InCluster: true,
-    }
-    k.Configure()
-    k.Connect()
-    akgPods := k.AkgPods()
-    log.Printf("Akg Pods: %s", akgPods)
+	// k8s
+	k := &k8s.K8s{
+		InCluster: true,
+	}
+	k.Configure()
+	k.Connect()
+	akgPods := k.AkgPods()
+	log.Printf("Akg Pods: %s", akgPods)
 
-    // gin
-    ws := &webserver.WebServer{}
-    ws.Start()
+	// gin
+	ws := &webserver.WebServer{}
+	ws.Start()
 }
-
