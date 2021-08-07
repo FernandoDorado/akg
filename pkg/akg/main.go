49cd97d3963293237bd041ec6984f46fc4dcb1dd
Adam K Gray <adamkgray@pm.me>
2021-08-07 19:36:51 -0400

    style, handle curl requests with JSON response

--- pkg/akg/main.go
+++ pkg/akg/main.go
@@ -2,18 +2,24 @@ package main
 
 import (
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	// http
 	e := gin.Default()
 	e.LoadHTMLGlob("templates/*")
 	e.GET("/", func(c *gin.Context) {
+		if strings.Contains(c.Request.Header.Get("User-Agent"), "curl") {
+			c.JSON(http.StatusOK, gin.H{
+				"Name":  "Adam K Gray",
+				"About": "Solutions Architecture, Data Science",
+			})
+			return
+		}
 		c.HTML(http.StatusOK, "index.tmpl", gin.H{})
 	})
-
 	e.GET("/health/live", func(c *gin.Context) {})
 	e.Run()
 }
