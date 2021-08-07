929ad68f32d3e612c0b2c649e71c02529393485e
Adam K Gray <adamkgray@pm.me>
2021-08-07 12:37:21 -0400

    remove k8s replica API

--- pkg/akg/go.mod
+++ pkg/akg/go.mod
@@ -2,9 +2,4 @@ module akg
 
 go 1.16
 
-require (
-	github.com/adamkgray/dok8cert v0.0.0-20210621122010-57a85ce38c89
-	github.com/gin-gonic/gin v1.7.2
-	k8s.io/apimachinery v0.21.2
-	k8s.io/client-go v0.21.2
-)
+require github.com/gin-gonic/gin v1.7.2
--- pkg/akg/main.go
+++ pkg/akg/main.go
@@ -1,22 +1,12 @@
 package main
 
 import (
-	"fmt"
-	"log"
 	"net/http"
-	"os"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	k := &k8s{}
-	k.InCluster = (os.Getenv("IN_CLUSTER") == "true")
-	k.CloudProvider = os.Getenv("CLOUD_PROVIDER")
-	if ok, _ := k.configure(); ok {
-		k.connect()
-	}
-
 	// http
 	e := gin.Default()
 	e.LoadHTMLGlob("templates/*")
@@ -24,56 +14,6 @@ func main() {
 		c.HTML(http.StatusOK, "index.tmpl", gin.H{})
 	})
 
-	e.GET("/api/v1/k8s/reconfigure", func(c *gin.Context) {
-		ok, err := k.configure()
-		if !ok {
-			msg := fmt.Sprintf("failed to reconfigure kubernetes client: %s", err)
-			log.Print(msg)
-			c.JSON(500, gin.H{
-				"error": msg,
-			})
-			return
-		}
-		msg := "reconfigured kubernetes client"
-		log.Print(msg)
-		c.JSON(200, gin.H{})
-	})
-
-	e.GET("/api/v1/k8s/reconnect", func(c *gin.Context) {
-		ok, err := k.connect()
-		if !ok {
-			msg := fmt.Sprintf("failed to reconnect kubernetes client: %s", err)
-			log.Print(msg)
-			c.JSON(500, gin.H{
-				"error": msg,
-			})
-			return
-		}
-		msg := "reconnected kubernetes client"
-		log.Print(msg)
-		c.JSON(200, gin.H{})
-	})
-
-	e.GET("/api/v1/k8s/apps", func(c *gin.Context) {
-		if !k.Ready {
-			c.JSON(500, gin.H{
-				"error": "kubernetes client is not ready",
-			})
-			return
-		}
-
-		apps, err := k.apps()
-		if err != nil {
-			c.JSON(500, gin.H{
-				"error": err.Error(),
-			})
-			return
-		}
-
-		c.JSON(200, gin.H{
-			"apps": apps,
-		})
-	})
 	e.GET("/health/live", func(c *gin.Context) {})
 	e.Run()
 }
