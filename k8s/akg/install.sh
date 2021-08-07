31fb065e80ab21d52255357bbc1f8a73e7e2c1d5
Adam K Gray <adamkgray@pm.me>
2021-08-07 13:36:28 -0400

    update installation commands

--- k8s/akg/install.sh
+++ k8s/akg/install.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 kubectl apply -f vs.yml
-kubectl apply -f cluster-role.yml
-kubectl apply -f cluster-role-binding.yml
 kubectl apply -f sa.yml
 kubectl apply -f svc.yml
 kubectl apply -f deployment.yml
